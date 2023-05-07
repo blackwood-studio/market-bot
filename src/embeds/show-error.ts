0cdfde1816497c9e76f6eba5b9befccda3f25662
KuengNoel <74761380+KuengNoel@users.noreply.github.com>
2023-05-07 23:55:34 +0200

    Error Handling optimized (#25)
    * Error Handling optimized
    
    * Error Handling optimized

--- src/embeds/show-error.ts
+++ src/embeds/show-error.ts
@@ -1,6 +1,87 @@
 import { EmbedBuilder } from 'discord.js';
+import { Parameters } from '../general/parameters';
 
-export function show_error(description: string, tip: string): EmbedBuilder {
+export enum ErrorCode {
+    IS_BOT,
+    TARGET_IS_SOURCE,
+    INVALID_TICKER,
+    INVALID_MONEY_AMOUNT,
+    INVALID_ITEMS_AMOUNT,
+    ITEMS_ARE_FOR_SALE,
+    ITEMS_ARE_NOT_FOR_SALE,
+    HAS_NOT_ENOUGH_MONEY,
+    HAS_NOT_ENOUGH_ITEMS,
+    PROJECT_EXISTS,
+    PROJECT_NOT_EXISTS,
+    NOT_OWNER_OF_PROJECT,
+}
+
+export function show_error(error_code: ErrorCode, parameters: Parameters): EmbedBuilder {
+    let description: string;
+    let tip: string;
+
+    switch(error_code) {
+        case ErrorCode.IS_BOT:
+            description = `Option 'target' is invalid`,
+            tip = `Option 'target' can only be a real user`
+            break;
+
+        case ErrorCode.TARGET_IS_SOURCE:
+            description = `Option 'target' is invalid`,
+            tip = `Cannot choose yourself as 'target'`
+            break;
+
+        case ErrorCode.INVALID_TICKER:
+            description = `Option 'ticker' is invalid`;
+            tip = `Option 'ticker' must follow the pattern A-Z, 0-9, . and max length 28`;
+            break;
+
+        case ErrorCode.INVALID_MONEY_AMOUNT:
+            description = `Option 'money_amount' is invalid`;
+            tip = `Option 'money_amount' cannot be negative or zero`;
+            break;
+
+        case ErrorCode.INVALID_ITEMS_AMOUNT:
+            description = `Option 'items_amount' is invalid`;
+            tip = `Option 'items_amount' cannot be negative or zero`;
+            break;
+
+        case ErrorCode.ITEMS_ARE_FOR_SALE:
+            description = `Sale already exists`,
+            tip = `You need to cancel the sale of '${parameters.source_bundle.ticker}'`
+            break;
+
+        case ErrorCode.ITEMS_ARE_NOT_FOR_SALE:
+            description = `Items are not for sale`,
+            tip = `User '${parameters.target.username}' does not sell '${parameters.target_bundle.ticker}'`
+            break;
+
+        case ErrorCode.HAS_NOT_ENOUGH_MONEY:
+            description = `Not enough money`,
+            tip = `User '${parameters.source.username}' is ${(parameters.price - parameters.source.money_amount).toFixed(2)}$ short`;
+            break;
+
+        case ErrorCode.HAS_NOT_ENOUGH_ITEMS:
+            description = `Not enough items`,
+            tip = `User '${parameters.source.username}' is ${parameters.items_amount - (parameters.source_bundle.items_amount - parameters.source_bundle.items_amount_for_sale)} items short`
+            break;
+
+        case ErrorCode.PROJECT_EXISTS:
+            description = `Project does not exists`;
+            tip = `Project '${parameters.ticker}' could not be found`;
+            break;
+
+        case ErrorCode.PROJECT_NOT_EXISTS:
+            description = `Project already exists`;
+            tip = `Project '${parameters.ticker}' already exists`;
+            break;
+
+        case ErrorCode.NOT_OWNER_OF_PROJECT:
+            description = `Missing owner rights`;
+            tip = `User '${parameters.source.username}' is not owner of '${parameters.ticker}'`;
+            break;
+    }
+
     return new EmbedBuilder()
     .setColor(0xe33900)
     .addFields(
--- src/interactions/sell-items.ts
+++ src/interactions/sell-items.ts
@@ -1,70 +1,73 @@
 import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
 import { load_user } from '../general/load-user';
 import { bundles, logger } from '../static';
-import { does_not_project_exists, has_not_enough_items, is_items_amount_invalid, is_money_amount_invalid, is_ticker_invalid, items_are_for_sale } from '../general/validator';
-import { show_error } from '../embeds/show-error';
+import { does_project_not_exists, has_not_enough_items, is_items_amount_invalid, is_money_amount_invalid, is_ticker_invalid, items_are_for_sale } from '../general/validator';
+import { ErrorCode, show_error } from '../embeds/show-error';
 import { load_bundle } from '../general/load-bundle';
 import { show_success } from '../embeds/show-success';
+import { Parameters } from '../general/parameters';
 
 export async function sell_items(interaction: ChatInputCommandInteraction): Promise<EmbedBuilder> {
-    const user = await load_user(interaction.user);
-    const ticker = interaction.options.getString('ticker');
-    const items_amount = interaction.options.getInteger('items_amount');
-    const price_per_item = interaction.options.getNumber('price_per_item');
-    const user_bundle = await load_bundle(user, ticker);
+    const parameters: Parameters = {}
 
-    if (is_ticker_invalid(ticker)) {
-        logger.error(`New sell items request ... FAILED`);
+    parameters.source = await load_user(interaction.user);
+    parameters.ticker = interaction.options.getString('ticker');
+    parameters.items_amount = interaction.options.getInteger('items_amount');
+    parameters.source_bundle = await load_bundle(parameters.source, parameters.ticker);
+    parameters.price_per_item = interaction.options.getNumber('price_per_item')
+
+    if (is_ticker_invalid(parameters.ticker)) {
+        logger.error(`New add items request ... FAILED`);
         return show_error(
-            `Option 'ticker' is invalid`,
-            `Option 'ticker' must follow the pattern A-Z, 0-9, . and max length 28`
+            ErrorCode.INVALID_TICKER,
+            parameters
         );
     }
 
-    if (is_items_amount_invalid(items_amount)) {
+    if (is_items_amount_invalid(parameters.items_amount)) {
         logger.error(`New sell items request ... FAILED`);
         return show_error(
-            `Option 'items_amount' is invalid`,
-            `Option 'items_amount' cannot be negative or zero`
+            ErrorCode.INVALID_ITEMS_AMOUNT,
+            parameters
         );
     }
 
-    if (is_money_amount_invalid(price_per_item)) {
+    if (is_money_amount_invalid(parameters.price_per_item)) {
         logger.error(`New sell items request ... FAILED`);
         return show_error(
-            `Option 'price_per_item' is invalid`,
-            `Option 'price_per_item' cannot be negative or zero`
+            ErrorCode.INVALID_MONEY_AMOUNT,
+            parameters
         );
     }
 
-    if (await does_not_project_exists(ticker)) {
+    if (await does_project_not_exists(parameters.ticker)) {
         logger.error(`New sell items request ... FAILED`);
         return show_error(
-            `Project does not exists`,
-            `Project '${ticker}' could not be found`
+            ErrorCode.PROJECT_NOT_EXISTS,
+            parameters
         );
     }
 
-    if (items_are_for_sale(user_bundle)) {
+    if (items_are_for_sale(parameters.source_bundle)) {
         logger.error(`New sell items request ... FAILED`);
         return show_error(
-            `Sale already exists`,
-            `You need to cancel the sale of '${user_bundle.ticker}'`
+            ErrorCode.ITEMS_ARE_FOR_SALE,
+            parameters
         );
     }
 
-    if (has_not_enough_items(user_bundle, items_amount)) {
+    if (has_not_enough_items(parameters.source_bundle, parameters.items_amount)) {
         logger.error(`New sell items request ... FAILED`);
         return show_error(
-            `Not enough items`,
-            `User '${user.username}' is ${items_amount - (user_bundle.items_amount - user_bundle.items_amount_for_sale)} items short`
+            ErrorCode.HAS_NOT_ENOUGH_ITEMS,
+            parameters
         );
     }
 
-    user_bundle.items_amount_for_sale = items_amount;
-    user_bundle.price_per_item = price_per_item;
+    parameters.source_bundle.items_amount_for_sale = parameters.items_amount;
+    parameters.source_bundle.price_per_item = parameters.price_per_item;
     
-    await bundles.set(`${user.id}::${ticker}`, user_bundle);
+    await bundles.set(`${parameters.source.id}::${parameters.ticker}`, parameters.source_bundle);
 
     logger.info(`New sell items request ... SUCCESS`);
     return show_success(`Sale has successfully been posted`);
